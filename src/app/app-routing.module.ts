d883570403628a6bcb103c87666ba94645e9b7bf
vado <86899022+JoseVado@users.noreply.github.com>
2022-12-06 15:10:35 -0600

    terminación de la pagina aviso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AvisoComponent } from './componentes/cliente/aviso/aviso.component';
 import { HomeComponent } from './componentes/cliente/home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  { path: '', component: HomeComponent },
+  { path: 'aviso', component: AvisoComponent },
 ];
 
 @NgModule({
