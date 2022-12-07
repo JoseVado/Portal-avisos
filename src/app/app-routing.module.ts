274b5fab6efd19f63089b38debd945da8c715590
vado <86899022+JoseVado@users.noreply.github.com>
2022-12-06 19:55:31 -0600

    agregar rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AvisoComponent } from './componentes/cliente/aviso/aviso.component';
 import { HomeComponent } from './componentes/cliente/home/home.component';
+import { LoginComponent } from './componentes/admin/login/login.component';
+import { TablaAvisosComponent } from './componentes/admin/tabla-avisos/tabla-avisos.component';
+import { SubirModificarAvisoComponent } from './componentes/admin/subir-modificar-aviso/subir-modificar-aviso.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'aviso', component: AvisoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'tabla-avisos', component: TablaAvisosComponent },
+  { path: 'subir-aviso', component: SubirModificarAvisoComponent },
 ];
 
 @NgModule({
